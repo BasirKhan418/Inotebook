7971711073a0913a1fb992f6e51e647b056bd873
Basir Khan <khanbasir5555@gmail.com>
2023-05-27 00:04:20 +0530

    issue fixed

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 // const uri ="mongodb://localhost:27017";
 
-const server = '127.0.0.1:27017';
+const server = 'mongodb+srv://basir:basir418@cluster0.ydjhxux.mongodb.net/inotebook';
 const database = 'inotebook';
 const connectToMongo=async()=>{
-    await mongoose.connect(`mongodb://${server}/${database}`)
+    await mongoose.connect(`${server}`)
     .then(()=>{
         console.log("connected to mongo successfully!!")
     }).catch((err)=>{
